a6eaf32815951f2744553f85473c907522a00c0f
Lynla13 <lynla1318@gmail.com>
2023-05-06 14:34:03 +0700

    finniz mvc model

---
+++ src/confgs/connect_db.js
@@ -0,0 +1,14 @@
+import mysql from 'mysql2';
+
+// const pool = mysql.createPool({
+//     host: 'sql12.freemysqlhosting.net',
+//     user: 'sql12597919',
+//     password: 'pbVbtEz5k5',
+//     database: 'sql12597919',
+// })
+
+// export default pool;
+
+// host: 'localhost',
+// user: 'root',
+// database: 'nodejs-basic',
